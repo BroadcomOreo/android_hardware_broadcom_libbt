7b55a4801316992d8ec8d5dc6f6804594452259f
sostk <poudelswastik@gmail.com>
2022-08-12 15:19:41 +1000

    Update userial_vendor.c

--- src/userial_vendor.c
+++ src/userial_vendor.c
@@ -269,6 +269,13 @@ int userial_vendor_open(tUSERIAL_CFG *p_cfg)
     tcsetattr(vnd_userial.fd, TCSANOW, &vnd_userial.termios);
 
 #if (BT_WAKE_VIA_USERIAL_IOCTL==TRUE)
+    // TODO: check for breakage on tuna (Galaxy Nexus). It defines this,
+    //       but does not contain the kernel code to support it.
+
+    // Switch to N_BRCM_HCI line disclipline for ioctl to work
+    ldisc = 25; // N_BRCM_HCI
+    ioctl(vnd_userial.fd, TIOCSETD, &ldisc);
+    
     userial_ioctl_init_bt_wake(vnd_userial.fd);
 #endif
 
